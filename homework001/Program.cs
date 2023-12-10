71d9994e11a6089fb7dba3b6bd26a92c20dd46f5
Wavednight <wavednight@gmail.com>
2023-12-10 18:28:48 +0700

    Задача1

--- homework001/Program.cs
+++ homework001/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿char[,] charArray = new char[,]
+{
+    { 'H', 'e', 'l', 'l', 'o' },
+    { 'W', 'o', 'r', 'l', 'd' }
+};
+
+string str = "";
+
+for (int i = 0; i < charArray.GetLength(0); i++)
+{
+    for (int j = 0; j < charArray.GetLength(1); j++)
+    {
+        str += charArray[i, j];
+    }
+}
+
+Console.WriteLine(str);
