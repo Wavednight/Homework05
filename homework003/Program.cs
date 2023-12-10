1c604e0b1c03ad6c5169ceb3a885cdffd6c34060
Wavednight <wavednight@gmail.com>
2023-12-10 18:36:53 +0700

    Задача2

--- homework003/Program.cs
+++ homework003/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string inputString = "aBcD1ef";
+bool isPalindrome = true;
+
+for (int i = 0; i < inputString.Length / 2; i++)
+{
+    if (inputString[i] != inputString[inputString.Length - 1 - i])
+    {
+        isPalindrome = false;
+        break;
+    }
+}
+
+if (isPalindrome)
+{
+    Console.WriteLine("Строка является палиндромом.");
+}
+else
+{
+    Console.WriteLine("Строка не является палиндромом.");
+}
